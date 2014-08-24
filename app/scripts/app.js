30fb4cbb36be6b0afef9636fca5839e776f0d5d1
Parker Lusk <parkerclusk@gmail.com>
2014-08-23 19:35:42 -0600

    update to angular 1.3 beta 19; change name to mechatronicsApp

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,33 +1,43 @@
 'use strict';
 
+angular.module('mechatronicsApp.controllers', []);
+angular.module('mechatronicsApp.services', []);
+angular.module('mechatronicsApp.directives', []);
+angular.module('mechatronicsApp.filters', []);
+
+
 /**
  * @ngdoc overview
- * @name mechAppApp
+ * @name mechatronicsApp
  * @description
- * # mechAppApp
+ * # mechatronicsApp
  *
  * Main module of the application.
  */
 angular
-  .module('mechAppApp', [
+.module('mechatronicsApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
+    'ngTouch',
+    'mechatronicsApp.controllers',
+    'mechatronicsApp.services',
+    'mechatronicsApp.directives',
+    'mechatronicsApp.filters'
+])
+.config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
+    .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
-      })
-      .when('/about', {
+    })
+    .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
-      })
-      .otherwise({
+    })
+    .otherwise({
         redirectTo: '/'
-      });
-  });
+    });
+});
